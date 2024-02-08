de7523083e03cda2bf772b17af02c22960b4d3cb
vbenv <tjdcksl20@gmail.com>
2024-02-08 10:31:23 +0900

    v0.2

--- day20.py
+++ day20.py
@@ -1,5 +1,13 @@
 def factorial(number) -> int:
-	pass
+	'''
+	factorail by repetition
+	:param number: number(int)
+	:return: factorial result (int)
+	'''
+	result = 1
+	for i in range(1, number+1):
+		result = result * i
+	return result
 
 
 def nCr(n, r) -> int:
