04ef0f39adf402b100762a5ff369a20cf0500b4f
vbenv <tjdcksl20@gmail.com>
2024-02-08 15:05:52 +0900

    v0.6 poly

--- day20.py
+++ day20.py
@@ -1,18 +1,32 @@
-import random
+def print_poly(f_x) -> str:
+    term = len(f_x) - 1
+    poly_expression = "f(x) = "
 
-answer = random.randint(1, 100)
-chance = 7
-i = 7
-while chance != 0:
-	chance -= 1
-	print(f'chance left {chance+1}')
-	guess= int(input("Input guess number : "))
-	if guess == answer:
-		print(f'You win. Answer is {answer}, {7-chance}번 만에 맞췄습니다.')
-		break
-	elif guess > answer:
-		print("Down")
-	else:
-		print("Up")
-else:
-	print("Game Over")
+    for i in range(len(fx)):
+        coefficient = f_x[i]
+
+        if coefficient >= 0:
+            poly_expression = poly_expression + "+"
+        poly_expression = poly_expression + f'{coefficient}x^{term} '
+        term = term - 1
+
+    return poly_expression
+
+
+def calculation_poly(x_value, f_x) -> int:
+    return_value = 0
+    term = len(f_x) - 1
+
+    for i in range(len(fx)):
+        coefficient = f_x[i]
+        return_value += coefficient * pow(x_value, term)
+        term = term - 1
+
+    return return_value
+
+
+fx = [2, 3, 4, 0, -9]
+
+if __name__ == "__main__":
+    print(print_poly(fx))
+    print(calculation_poly(int(input("x 값 : ")), fx))
