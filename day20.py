29f33b655b4d163bcbb6049d65e1924cbfa1448c
vbenv <tjdcksl20@gmail.com>
2024-02-08 10:41:37 +0900

    v0.3 recursion

--- day20.py
+++ day20.py
@@ -1,13 +1,26 @@
+# def factorial(number) -> int:
+# 	'''
+# 	factorail by repetition
+# 	:param number: number(int)
+# 	:return: factorial result (int)
+# 	'''
+# 	result = 1
+# 	for i in range(1, number+1):
+# 		result = result * i
+# 	return result
+
+# 재귀함수는 반복문이 사용되는 것이 아닌 자신의 함수를 계속 불러오는 형식으로 사용
+# 성능은 무지하게 느림.
 def factorial(number) -> int:
 	'''
-	factorail by repetition
+	factorial by recursion
 	:param number: number(int)
-	:return: factorial result (int)
+	:return: factoral result (int)
 	'''
-	result = 1
-	for i in range(1, number+1):
-		result = result * i
-	return result
+	if number == 1:
+		return 1
+	else:
+		return number * factorial(number - 1)
 
 
 def nCr(n, r) -> int:
@@ -23,6 +36,8 @@ def nCr(n, r) -> int:
 
 
 if __name__ == "__main__": #메인 파일의 안쪽 코드를 사용하는 모듈
-	n = int(input("Input n : "))
-	r = int(input("Input r : "))
-	print(f"{n}C{r} = {nCr(n, r)}")
+	# n = int(input("Input n : "))
+	# r = int(input("Input r : "))
+	# print(f"{n}C{r} = {nCr(n, r)}")
+	f = int(input())
+	print(factorial(f))
