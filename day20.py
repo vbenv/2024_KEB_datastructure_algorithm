03a07782b85d65c07baeb34e2ef943add9ed3371
vbenv <tjdcksl20@gmail.com>
2024-02-08 11:25:58 +0900

    v0.5 ocp

--- day20.py
+++ day20.py
@@ -1,5 +1,5 @@
 import mymath
-
+import time
 
 
 
@@ -7,6 +7,10 @@ import mymath
 if __name__ == "__main__": #메인 파일의 안쪽 코드를 사용하는 모듈
 	n = int(input("Input n : "))
 	r = int(input("Input r : "))
+	#start = time.time()
 	print(f"{n}C{r} = {mymath.nCr(n, r)}")
+	#end = time.time()
+	#print(f"소요시간 : {end - start}")
 	# f = int(input())
 	# print(mymath.factorial(f))
+
--- mymath.py
+++ mymath.py
@@ -1,34 +1,48 @@
+import time
+
+
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print(f"time elapsed : {end - start}")
+        return result
+    return wrapper
+
+
 # def factorial(number) -> int:
-# 	'''
-# 	factorail by repetition
-# 	:param number: number(int)
-# 	:return: factorial result (int)
-# 	'''
-# 	result = 1
-# 	for i in range(1, number+1):
-# 		result = result * i
-# 	return result
+#     '''
+#     factorial by repetition
+#     :param number: number (int)
+#     :return: factorial result (int)
+#     '''
+#     result = 1
+#     for i in range(1, number+1):
+#         result = result * i
+#     return result
 
-# 재귀함수는 반복문이 사용되는 것이 아닌 자신의 함수를 계속 불러오는 형식으로 사용
-# 성능은 무지하게 느림.
+
 def factorial(number) -> int:
-	'''
-	factorial by recursion
-	:param number: number(int)
-	:return: factoral result (int)
-	'''
-	if number == 1:
-		return 1
-	else:
-		return number * factorial(number - 1)
+    '''
+    factorial by recursion
+    :param number: number (int)
+    :return: factorial result (int)
+    '''
+    if number == 1:
+        return 1
+    else:
+        return number * factorial(number - 1)
 
-def nCr(n, r) -> int:
-	'''
-	조합 함수
-	:param n: 전체 갯수
-	:param r: 뽑는 갯수
-	:return:
-	'''
-	numerator = factorial(n)
-	denominator = factorial(n-r) * factorial(r)
-	return int(numerator / denominator)
+
+@timer
+def nCr(n, r) -> int:  # SRP, OCP
+    '''
+    combination function
+    :param n:
+    :param r:
+    :return:
+    '''
+    numerator = factorial(n)
+    denominator = factorial(n-r) * factorial(r)
+    return int(numerator / denominator)
