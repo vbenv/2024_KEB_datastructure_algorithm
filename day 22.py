34742df743982043a441db2c066cbfc2faaaf157
vbenv <tjdcksl20@gmail.com>
2024-02-14 16:55:20 +0900

    v1.4 recursion

--- day 22.py
+++ day 22.py
@@ -1,32 +1,33 @@
-# def decimal_to_octal(number: int) -> str:
-#     """
-#     decimal number to octal number
-#     :param number: integer (base dec)
-#     :return: string (base octal)
-#     """
-#     octal = ''
-#     while number > 0:
-#         octal = str(number % 8) + octal
-#         number = number // 8
-#     return octal
-#
-# #
-# n = int(input("Input decimal number : "))
-# print(decimal_to_octal(n))
-
-
-# 재귀함수를 사용하면 속도도 빠르고 직관적임
-def decimal_to_octal(number: int) -> str:
+def fibo_recursion(number: int) -> int:
     """
-    decimal number to octal number
-    :param number: integer (base dec)
-    :return: string (base octal)
+    fibonacci function by recursion.
+    :param number: integer number
+    :return: integer number
     """
-    if number < 8:
-        return str(number)
-    else:
-        return decimal_to_octal(n//8) + str(n % 8)
+    if number == 0:
+        return 0
+    elif number == 1:
+        return 1
+    else :
+        return fibo_recursion(number-1) + fibo_recursion(number-2)
 
 
-n = int(input("Input decimal number : "))
-print(decimal_to_octal(n))
+n = int(input("Input first number : "))
+# for i in range(0, n) :
+#     print(i)
+#     print(fibo_recursion(i), end=' ')
+
+
+def fibo_repetition(number : int) -> int:
+    a = 0
+    b = 1
+    for _ in range(number):
+        # a = b
+        # b= a + b // 임시변수 temp or 언패킹으로 문제 해결
+        a, b = b, a + b
+    return a
+
+print(fibo_repetition(n))
+
+
+
