69ecf73c0bcfe2ab8d13ddee348095bc3c8ab7b4
vbenv <tjdcksl20@gmail.com>
2024-02-14 16:08:58 +0900

    v1.2 recursion

--- day 22.py
+++ day 22.py
@@ -1,27 +1,15 @@
-# 호출하는 함수
-def dfs(g, i, visited):#g : graph, i : 횟수, visited: 방문 기록
-    visited[i] = 1 #0번을 초기화
-    print(f'{name_ary[i]}', end=' ')
-    #print(chr(ord('A')+i), end = ' ')
-    #print(i, end=' ')
-    for j in range(len(g)):
-        if g[i][j] == 1 and not visited[j]:
-            dfs(g, j, visited)
-
-name_ary = ["문별", "솔라", "휘인", "쯔위", '선미', "화사"]
-mb, sl, hi, zz, sm, hs = 0, 1, 2, 3, 4, 5
-graph = [
-    [0, 1, 1, 0, 0, 0],  #mb
-    [1, 0, 0, 1, 0, 0],  #sl
-    [1, 0, 0, 1, 0, 0],  #hi
-    [0, 1, 1, 0, 1, 1],  #zz
-    [0, 0, 0, 1, 0, 1],  #sm
-    [0, 0, 0, 1, 1, 0]   #hs
-]
+def decimal_to_octal(number: int) -> str:
+    """
+    decimal number to octal number
+    :param number: integer (base dec)
+    :return: string (base octal)
+    """
+    octal = ''
+    while number > 0:
+        octal = str(number % 8) + octal
+        number = number // 8
+    return octal
 
 
-# 전역변수
-visited = [0] * len(graph)
-#visited = [0 for _ in range(len(graph))] #위랑 같은 코드
-
-dfs(graph, 0, visited)
+n = int(input("Input decimal number : "))
+print(decimal_to_octal(n))
