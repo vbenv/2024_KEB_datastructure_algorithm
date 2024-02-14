0c9650cbc27a5724f78a93badf9321bbf7355af9
vbenv <tjdcksl20@gmail.com>
2024-02-14 14:25:14 +0900

    v1.0 DFS

--- day 22.py
+++ day 22.py
@@ -1,21 +1,21 @@
 # 호출하는 함수
-def dfs(g, v, visited):
-    visited[v] = True #0번을 초기화
-    print(chr(ord('A')+v), end = ' ')
-    #print(v, end=' ')
-    for i in range(len(graph)):
-        if g[v][i] == True and not visited[i]:
-            dfs(g, i, visited)
+def dfs(g, i, visited):#g : graph, i : 횟수, visited: 방문 기록
+    visited[i] = 1 #0번을 초기화
+    print(chr(ord('A')+i), end = ' ')
+    #print(i, end=' ')
+    for j in range(len(g)):
+        if g[i][j] == 1 and not visited[j]:
+            dfs(g, j, visited)
 
 graph = [
-    [0, 1, 0, 1, 0],
-    [1, 1, 1, 1, 0],
-    [0, 1, 0, 0, 1],
-    [1, 1, 0, 0, 1],
-    [0, 0, 1, 1, 0]
+    [0, 0, 1, 1, 0],
+    [0, 0, 1, 0, 0],
+    [1, 1, 0, 1, 1],
+    [1, 0, 1, 0, 0],
+    [0, 0, 1, 0, 0]
 ]
 
 
 # 전역변수
-visited = [False] * len(graph)
+visited = [0] * len(graph)
 dfs(graph, 0, visited)
