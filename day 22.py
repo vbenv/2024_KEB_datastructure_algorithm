b9accffe5e024cdef6afc696e70d38514b5e028d
vbenv <tjdcksl20@gmail.com>
2024-02-14 17:32:12 +0900

    v1.5 recursion

--- day 22.py
+++ day 22.py
@@ -8,26 +8,51 @@ def fibo_recursion(number: int) -> int:
         return 0
     elif number == 1:
         return 1
-    else :
-        return fibo_recursion(number-1) + fibo_recursion(number-2)
+    else:
+        return fibo_recursion(number - 1) + fibo_recursion(number - 2)
 
 
-n = int(input("Input first number : "))
-# for i in range(0, n) :
-#     print(i)
-#     print(fibo_recursion(i), end=' ')
-
-
-def fibo_repetition(number : int) -> int:
+def fibo_repetition(number: int) -> int:
+    """
+    fibonacci function by repetition.
+    :param number: integer number
+    :return: integer number
+    """
     a = 0
     b = 1
     for _ in range(number):
-        # a = b
-        # b= a + b // 임시변수 temp or 언패킹으로 문제 해결
         a, b = b, a + b
     return a
 
-print(fibo_repetition(n))
+
+memo = [None for _ in range(100)]
+def fibo_memoization(number: int, memo: list) -> int:
+    """
+    fibonacci function by recursion with memoization.
+    :param number: integer number
+    :return: integer number
+    """
+    if memo[number] is not None:
+        return memo[number]
+    if number < 2:
+        result = number
+    else:
+        result = fibo_memoization(number-1, memo) + fibo_memoization(number-2, memo)
+        memo[number] = result
+    return result
 
 
 
+n = int(input("Input number : "))
+
+for i in range(0, n):
+    print(i)
+    print(fibo_memoization(i, memo))
+print("===========================")
+for i in range(0, n):
+    print(i)
+    print(fibo_recursion(i))
+print("===========================")
+for i in range(0, n):
+    print(i)
+    print(fibo_repetition(i))
