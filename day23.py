a7c43e1e0125515924817adcb11db7da5d2ca952
vbenv <tjdcksl20@gmail.com>
2024-02-15 11:22:24 +0900

    v1.8 fibonacci after gui

--- day23.py
+++ day23.py
@@ -1,8 +1,6 @@
 import tkinter as tk
 
-memo = [0 if i == 0 else i if i  == 1 else None for i in range(100)]
-#memo = [0 if i == 0 else i if i  == 1 else None for i in range(100)]
-#memo = [0, 1] + [None] * (100-1)
+memo = [0 if i == 0 else 1 if i == 1 else None for i in range(100)]
 def fibo_memoization(number: int) -> int:
     """
     fibonacci function by recursion with memoization.
@@ -12,29 +10,32 @@ def fibo_memoization(number: int) -> int:
     global memo
     if memo[number] is not None:
         return memo[number]
-    else:
-        result = fibo_memoization(number-1) + fibo_memoization(number-2)
-        memo[number] = result
+    result = fibo_memoization(number-1) + fibo_memoization(number-2)
+    memo[number] = result
     return result
 
+def process_fibonacci():
+    number = int(en_input_number.get())
+    lbl_display_fibonacci_result.config(text=f"{number}! = {fibo_memoization(number)}")
 
 
-# create window object
-w = tk.Tk()
-w.title("Fibonacci")
-w.geometry("300x150")
+if __name__ == "__main__":
+    w = tk.Tk()  # create window object
+    w.title("Fibonacci")
+    w.geometry("250x100")
 
+    # create widget
+    lbl_display_fibonacci_result = tk.Label(w, text='Fibonacci by memoization')
+    en_input_number = tk.Entry(w)
+    btn_click = tk.Button(w, text="Click", command=process_fibonacci)  # bind function
 
-# create widget
-lbl_display_fibonacci_result = tk.Label(w, text = 'Fibonacci by memoization')
-en_input_number = tk.Entry(w)
-btn_click = tk.Button(w, text = "Click")
+    # layout
+    lbl_display_fibonacci_result.pack()
+    en_input_number.pack(fill="x")
+    btn_click.pack(fill="x")
 
-# layout - 배치
-lbl_display_fibonacci_result.pack()
-en_input_number.pack(fill = "x")
-btn_click.pack(fill = "x")
+    en_input_number.focus()
+    w.mainloop()
 
-w.mainloop()    # x 누를 때까지 계속 실행.
-# n = int(input("Input number :x ")) # Input box로 대치
-# print(f"fibonacci({n}) = {fibo_memoization(n)}") # Lable로 대치
+# n = int(input("Input number : "))  # Input box
+# print(f"fibonacci({n}) = {fibo_memoization(n)}")  # Label
